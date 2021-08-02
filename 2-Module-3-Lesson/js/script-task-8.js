bd77ddd95ca19eda3ff92fbd9079bb6e8ac6a954
predator-1 <golubevmusic@gmail.com>
2021-08-02 21:33:03 +0300

    lesson updates

--- 2-Module-3-Lesson/js/script-task-8.js
+++ 2-Module-3-Lesson/js/script-task-8.js
@@ -1,3 +1,24 @@
 /**
  * Array-like object arguments
  */
+
+const argFunc = function () {
+  //for (const item of arguments) console.log(item);
+  //for (let i = 0; i < arguments.length; i++) console.log(arguments[i]);
+
+  const array = Array.from(arguments);
+  arguments[arguments.length] = 'new';
+  console.log(arguments);
+  console.log(array);
+};
+
+argFunc(1, 3, 2, 0);
+// console.log([1, 3, 2, 0]);
+
+/*
+"const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]
+*/
