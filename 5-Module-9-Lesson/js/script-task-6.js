c3381b3f14f40be36730f41e42340867efa6a460
predator-1 <golubevmusic@gmail.com>
2021-08-23 16:40:22 +0300

    lesson 9

---
+++ 5-Module-9-Lesson/js/script-task-6.js
@@ -0,0 +1,23 @@
+/**
+ * Что выведет консоль?
+ */
+const animal = { jumps: null };
+const rabbit = { jumps: true };
+
+// Object.setPrototypeOf(rabbit, animal);
+
+// console.log(1, rabbit.jumps);
+
+// delete rabbit.jumps;
+// console.log(2, rabbit.jumps);
+
+// delete animal.jumps;
+// console.log(3, rabbit.jumps);
+
+// function Rabbit() {}
+// Rabbit.prototype = { eats: true };
+// const rabbit2 = new Rabbit();
+// rabbit2.eats = false;
+
+// delete Rabbit.prototype.eats;
+// console.log(4, rabbit2.eats);
