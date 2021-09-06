d72a08f50386722c77426d1d12574bc7edb989d9
predator-1 <golubevmusic@gmail.com>
2021-09-06 15:30:55 +0300

    QnA 1-6

---
+++ Questions-1-6/js/script-task-5.js
@@ -0,0 +1,25 @@
+// Отсортируй массив по height, если они одинаковые - то по age
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+const users = [
+  {
+    firstName: 'Bobby',
+    height: 180,
+    age: 25,
+  },
+  {
+    firstName: 'Chris',
+    height: 160,
+    age: 20,
+  },
+  {
+    firstName: 'Peter',
+    height: 180,
+    age: 15,
+  },
+  {
+    firstName: 'Jimmy',
+    height: 170,
+    age: 30,
+  },
+];
