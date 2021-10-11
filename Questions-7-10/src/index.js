18fe131e4a56e9b5019bcda796706f52ea15e33b
predator-1 <golubevmusic@gmail.com>
2021-10-11 19:25:52 +0300

    qna 7-10

---
+++ Questions-7-10/src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+/**
+ * Create bank manager panel
+ * Manager can add user with fields 'Firstname' 'Lastname'. Field 'Status' will be init by default value
+ * Status has values 'Pending', 'Approved', 'Blocked'. Status 'Pending' is default
+ * Manager can change user status at any time
+ * Save all data to a disk
+ */
+
+import './styles.css';
